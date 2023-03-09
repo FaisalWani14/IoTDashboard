06d9904e38362f6fb18ce5b0f528c8229230d4e1
Yasu46 <y.suwa46@gmail.com>
2023-03-09 20:29:38 +0700

    iot to yasu

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-black text-h1">IOT</p>
+  <p class="text-black text-h1">yasu</p>
   <div>
     <div style="display: flex; justify-content: space-between">
       <ShowCard title="IN" :num="InVal" />
